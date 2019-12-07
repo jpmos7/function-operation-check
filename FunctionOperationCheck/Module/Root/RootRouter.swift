5ca5f3feae2711771e3922d851c02a521ec997a0
齊藤萌 <MOE@saitoumoe-no-MacBook-Air.local>
2019-12-07 20:44:01 +0900

    RootRouter.swift のタブアイテムを修正

--- FunctionOperationCheck/Module/Root/RootRouter.swift
+++ FunctionOperationCheck/Module/Root/RootRouter.swift
@@ -12,26 +12,26 @@ import UIKit
 class RootRouter {
 
     func presentFirstScreen(in window: UIWindow) {
-
         // 地図情報
         let mapView = R.storyboard.map.map()
-        //mapView?.tabBarItem = UITabBarItem(tabBarSystemItem: ., tag: 1)
         let mapNavigation = UINavigationController(rootViewController: mapView!)
 
         // 位置情報
         let locationView = R.storyboard.location.location()
-        locationView?.tabBarItem = UITabBarItem(tabBarSystemItem: .bookmarks, tag: 2)
         let locationNavigation = UINavigationController(rootViewController: locationView!)
 
+        // タブに詰める ViewController を追加する
         var viewControllers = [UIViewController]()
         viewControllers.append(mapNavigation)
         viewControllers.append(locationNavigation)
 
+        // tabBarController に viewControllers をセットする
         let tabBarController = R.storyboard.tab.tab()
-
         tabBarController?.setViewControllers(viewControllers, animated: false)
 
+        // rootViewController に tabBarController を設定
         window.rootViewController = tabBarController
+        // ウィンドウを表示し、キーウィンドウにする
         window.makeKeyAndVisible()
     }
 }
