fb4cc10e364a772a64c3ae031145530bf63dc41e
齊藤萌 <MOE@saitoumoe-no-MacBook-Air.local>
2019-12-07 22:36:03 +0900

    設定した緯度・経度、縮尺の地図を出力

--- FunctionOperationCheck/Module/Map/View/MapViewController.swift
+++ FunctionOperationCheck/Module/Map/View/MapViewController.swift
@@ -7,12 +7,33 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
 
     var presenter: MapPresentation?
 
+    /// 地図を表示するマップビュー
+    @IBOutlet weak var mapView: MKMapView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        // 東京駅の位置情報を設定（緯度: 35.681236 経度: 139.767125）
+        let latitude = 35.681236
+        let longitude = 139.767125
+
+        // 緯度・軽度を設定
+        let location = CLLocationCoordinate2DMake(latitude, longitude)
+        // マップビューに緯度・軽度を設定
+        mapView.setCenter(location, animated:true)
+
+        // 縮尺を設定
+        var region = mapView.region
+        region.center = location
+        region.span.latitudeDelta = 0.02
+        region.span.longitudeDelta = 0.02
+        // マップビューに縮尺を設定
+        mapView.setRegion(region,animated:true)
     }
 }
