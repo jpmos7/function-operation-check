d5b8d4d5217ada252f7472a14f927ca64da1bf95
齊藤萌 <MOE@saitoumoe-no-MacBook-Air.local>
2019-12-07 17:27:56 +0900

    地図情報の処理を削除

--- FunctionOperationCheck/Module/Map/View/MapViewController.swift
+++ FunctionOperationCheck/Module/Map/View/MapViewController.swift
@@ -7,29 +7,12 @@
 //
 
 import UIKit
-import MapKit
 
 class MapViewController: UIViewController {
-
-    @IBOutlet weak var mapView: MKMapView!
 
     var presenter: MapPresentation?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // 緯度・軽度を設定
-        let location:CLLocationCoordinate2D
-           = CLLocationCoordinate2DMake(35.68154, 139.752498)
-
-        mapView.setCenter(location,animated:true)
-
-        // 縮尺を設定
-        var region:MKCoordinateRegion = mapView.region
-        region.center = location
-        region.span.latitudeDelta = 0.02
-        region.span.longitudeDelta = 0.02
-
-        mapView.setRegion(region,animated:true)
     }
 }
