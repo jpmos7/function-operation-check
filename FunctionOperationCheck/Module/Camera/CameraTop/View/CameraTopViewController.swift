7950e0b8eeec2596ac110b30e0dc3d7de7687b66
齊藤萌 <MOE@saitoumoe-no-MacBook-Air.local>
2019-12-08 15:35:20 +0900

    カメラ機能についてコメントを修正

--- FunctionOperationCheck/Module/Camera/CameraTop/View/CameraTopViewController.swift
+++ FunctionOperationCheck/Module/Camera/CameraTop/View/CameraTopViewController.swift
@@ -20,16 +20,22 @@ class CameraTopViewController: UIViewController {
         let picker = UIImagePickerController()
         picker.sourceType = .camera
         picker.delegate = self
+        // UIImagePickerController カメラを起動する
         present(picker, animated: true, completion: nil)
     }
 }
 
 extension CameraTopViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    /// シャッターボタンを押下した際、確認メニューに切り替わる
+    /// - Parameters:
+    ///   - picker: ピッカー
+    ///   - info: 写真情報
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         let image = info[.originalImage] as! UIImage
-        //self.imageView.image = image
+        // "写真を使用"を押下した際、写真アプリに保存する
         UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+        // UIImagePickerController カメラが閉じる
         self.dismiss(animated: true, completion: nil)
     }
 }
