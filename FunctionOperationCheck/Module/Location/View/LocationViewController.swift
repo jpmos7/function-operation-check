c487dfcf9d04b22be01887649a124fc5a0faabe6
齊藤萌 <MOE@saitoumoe-no-MacBook-Air.local>
2019-12-08 12:28:00 +0900

    位置情報取得機能を実装しました。

--- FunctionOperationCheck/Module/Location/View/LocationViewController.swift
+++ FunctionOperationCheck/Module/Location/View/LocationViewController.swift
@@ -7,12 +7,66 @@
 //
 
 import UIKit
+import CoreLocation
 
 class LocationViewController: UIViewController {
 
     var presenter: LocationPresentation?
 
+    /// 緯度を表示するラベル
+    @IBOutlet weak var latitude: UILabel!
+    /// 経度を表示するラベル
+    @IBOutlet weak var longitude: UILabel!
+
+    // 緯度
+    var latitudeNow: String = ""
+    // 経度
+    var longitudeNow: String = ""
+
+    var locationManager: CLLocationManager!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        // ラベルに初期値を設定する
+        latitude.text = "デフォルト"
+        longitude.text = "デフォルト"
+
+        setupLocationManager()
+    }
+
+    /// "位置情報を取得"ボタンを押下した際、実機の位置情報を取得する
+    /// - Parameter sender: "位置情報を取得"ボタン
+    @IBAction func getLocationInfo(_ sender: Any) {
+        self.latitude.text = latitudeNow
+        self.longitude.text = longitudeNow
+    }
+
+    func setupLocationManager() {
+        locationManager = CLLocationManager()
+
+        // 権限をリクエスト
+        guard let locationManager = locationManager else { return }
+        locationManager.requestWhenInUseAuthorization()
+
+        // マネージャの設定
+        let status = CLLocationManager.authorizationStatus()
+        if status == .authorizedWhenInUse {
+            locationManager.delegate = self
+            //locationManager.distanceFilter = 10
+            locationManager.startUpdatingLocation()
+        }
+    }
+}
+
+extension LocationViewController: CLLocationManagerDelegate {
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations.first
+        let latitude = location?.coordinate.latitude
+        let longitude = location?.coordinate.longitude
+
+        self.latitudeNow = String(latitude!)
+        self.longitudeNow = String(longitude!)
     }
 }
